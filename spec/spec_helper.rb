ed21e812f4cfa26a1e5fe066b06f29cad14b60a9
Henning Koch <henning.koch@makandra.de>
2016-09-28 17:54:13 +0200

    Upgrade to Gemika 0.3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,21 +8,5 @@ require 'gemika'
 # Requires supporting files with custom matchers and macros, etc in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].sort.each {|f| require f}
 
-configurator = defined?(RSpec) ? RSpec : Spec::Runner
-configurator.configure do |config|
-
-  config.before(:each) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  if Gemika::Env.rspec_2_plus?
-    config.mock_with :rspec do |c|
-      c.syntax = [:should, :expect]
-    end
-
-    config.expect_with :rspec do |c|
-      c.syntax = [:should, :expect]
-    end
-  end
-
-end
+Gemika::RSpec.configure_clean_database_before_example
+Gemika::RSpec.configure_should_syntax
